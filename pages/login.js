df326f4ba1cddc5d6c36c71f183b4cdd9b8455cb
mattmoyka <mattmoyka@gmail.com>
2022-01-02 15:50:56 -0500

    next auth set up

---
+++ pages/login.js
@@ -0,0 +1,14 @@
+function Login() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Login
+
+export async function getServerSideProps() {
+  const providers = await getProviders[];
+
+}
